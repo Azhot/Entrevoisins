25a3f293dd140933cb6fd6dd509c03ce91e649b1
Azhot <francois.jouvelot@gmail.com>
2020-05-20 15:40:11 +0200

    Initial commit

---
+++ app/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoriteFragment.java
@@ -0,0 +1,102 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.events.FavoriteNeighbourEvent;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.List;
+
+
+public class FavoriteFragment extends Fragment {
+
+    private NeighbourApiService mApiService;
+    private List<Neighbour> mFavoriteNeighbours;
+    private RecyclerView mRecyclerView;
+
+    /**
+     * Create and return a new instance
+     * @return @{@link FavoriteFragment}
+     */
+    public static FavoriteFragment newInstance() {
+        return new FavoriteFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mApiService = DI.getNeighbourApiService();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorites_list, container, false);
+        Context context = view.getContext();
+        mRecyclerView = (RecyclerView) view;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        initList();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * Init the List of favorites
+     */
+    private void initList() {
+        mFavoriteNeighbours = mApiService.getFavorites();
+        if (mFavoriteNeighbours != null) {
+            mRecyclerView.setAdapter(new MyFavoriteRecyclerViewAdapter(mFavoriteNeighbours));
+        }
+    }
+
+    /**
+     * Refresh the List of favorites
+     */
+    private void deleteFromFavorites(Neighbour neighbour) {
+        mFavoriteNeighbours.remove(neighbour);
+        if (mRecyclerView.getAdapter() != null) {
+            mRecyclerView.getAdapter().notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Fired if the user clicks on a cancel button
+     * @param event
+     */
+    @Subscribe
+    public void onFavoriteNeighbour(FavoriteNeighbourEvent event) {
+        deleteFromFavorites(event.neighbour);
+    }
+}
